0f96bbdffbe3d34e21ef9800d6b4527acabb6f03
Andrew <andrewcrink@gmail.com>
2022-10-16 12:06:02 -0400

    fixed a few errors

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,9 +1,9 @@
 //this includes the vehicle class as a module
 const VehicleModule = require("./vehicle.js").Vehicle
 
-class Car extends VehicleModule () {
+class Car extends VehicleModule {
     constructor(make, model, year, color, mileage) {
-    super(make, model, year, color, mileage);
+        super(make, model, year, color, mileage);
         this.maximumPassengers = 5;
         this.passenger = 0;
         this.numberOfWheels = 4;
@@ -23,9 +23,15 @@ class Car extends VehicleModule () {
     }
 }
 
-    scheduleService() {
-        if (this.mileage > 30000) {
+    scheduleServiceNow(mileage) {
+        if (mileage > 30000) {
             this.scheduleService = true
+            console.log ('Schedule Service Now!')
+            return this.scheduleService;
+        }
+        else {
+            this.scheduleService = false
+            console.log ('No Service Needed')
             return this.scheduleService;
         }
     }
@@ -45,5 +51,11 @@ class Car extends VehicleModule () {
 
 
 //this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
+let mercury = new Car ("Mecury", "A28 Sedan", "1965", "Blue", "0");
+console.log(mercury);
+
+mercury.loadPassenger(6);
+mercury.start();
+mercury.scheduleServiceNow(30001);
+mercury.stop()
+
